e111fb544676381cc15d59c032a42ebbdfca6bd4
MadisonViaLux <madison.s.mckown@gmail.com>
2019-08-08 14:50:44 -0700

    setting up the data in the api

--- api/server.js
+++ api/server.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const server = express();
 
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        greeting: 'Hello there.',
+        reply: 'General Kenobi...'
+    })
+})
+
+
+
+
 module.exports = server
